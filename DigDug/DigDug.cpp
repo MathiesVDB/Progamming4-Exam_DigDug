2fb28242a57e99c952fc6e094ca3cbd2a098b4ed
Mathies Van den Bulcke <mathies.van.den.bulcke@student.howest.be>
2025-03-26 17:05:50 +0100

    Followed instructions but does not work yet

---
+++ DigDug/DigDug.cpp
@@ -0,0 +1,20 @@
+#if _DEBUG
+#if __has_include(<vld.h>)
+#include <vld.h>
+#endif
+#endif
+
+#include "Minigin.h"
+#include "SceneManager.h"
+
+using namespace dae;
+void load()
+{
+	auto& scene = dae::SceneManager::GetInstance().CreateScene("DigDug");
+}
+
+int main(int, char* []) {
+	dae::Minigin engine("../Data/");
+	engine.Run(load);
+	return 0;
+}
